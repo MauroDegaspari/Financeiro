4b619893f2a9a56b9a3a4e3560378d3a61bb49da
maurodegaspari <mauro_degaspari@hotmail.com>
2021-06-21 21:23:09 -0300

    correção backend

--- Finanças-backend/src/main/resources/data.sql
+++ Finanças-backend/src/main/resources/data.sql
@@ -1,9 +1,9 @@
-INSERT INTO tb_vendedor(USUARIO) VALUES ('MAURO');
-INSERT INTO tb_vendedor(USUARIO) VALUES ('NATALY');
+INSERT INTO tb_vendedor(nome) VALUES ('MAURO');
+INSERT INTO tb_vendedor(nome) VALUES ('NATALY');
 
-INSERT INTO tb_vendas(id,vendas,soma) VALUES (2, 45, 32.90,);
-INSERT INTO tb_vendas(id,vendas,soma) VALUES (1, 21, 65.90,);
-INSERT INTO tb_vendas(id,vendas,soma) VALUES (2, 87, 76.90,);
-INSERT INTO tb_vendas(id,vendas,soma) VALUES (1, 423, 12.90,);
-INSERT INTO tb_vendas(id,vendas,soma) VALUES (2, 55, 321.90,);
-INSERT INTO tb_vendas(id,vendas,soma) VALUES (1, 68, 123.90,);
+INSERT INTO tb_vendas(id_vendedor,venda,soma) VALUES (2, 45, 32.90);
+INSERT INTO tb_vendas(id_vendedor,venda,soma) VALUES (1, 21, 65.90);
+INSERT INTO tb_vendas(id_vendedor,venda,soma) VALUES (2, 87, 76.90);
+INSERT INTO tb_vendas(id_vendedor,venda,soma) VALUES (1, 423, 12.90);
+INSERT INTO tb_vendas(id_vendedor,venda,soma) VALUES (2, 55, 321.90);
+INSERT INTO tb_vendas(id_vendedor,venda,soma) VALUES (1, 68, 123.90);
