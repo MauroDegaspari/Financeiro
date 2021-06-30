b19e6594e4c5433c95621dd27592964d57715377
maurodegaspari <mauro_degaspari@hotmail.com>
2021-06-29 21:17:29 -0300

    finalização de backend, implementação de DTO somas de vendas de cada vendedor.

--- Finanças-frontend/src/pages/Dashboard/index.tsx
+++ Finanças-frontend/src/pages/Dashboard/index.tsx
@@ -1,13 +1,24 @@
+import { useState, useEffect } from "react";
 import Footer from "../../components/Footer/footer";
 import NavBar from "../../components/Navbar";
 import DataTable from "../../components/Table/datatable";
+import api from "../../services/api";
 
 const Dashboard = () => {
+    const [totalConnecitons, setTotalConnections] = useState(0);
+
+    useEffect(() =>{
+        api.get('/vendas').then(response => {
+        const {total} = response.data
+        setTotalConnections(total); 
+        })
+    }, []);
+
     return (
         <>
             <NavBar />
             <div className="container">
-                <h1 className="text-primary"> minha Dashboard...</h1>
+                <h1 className="text-primary">  Dashboard...</h1>
                 <DataTable />
             </div>
 
